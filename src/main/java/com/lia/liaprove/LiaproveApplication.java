b7eddd1ad5555d1e54720ce6a79acf496d0d3324
Vinícius Luquini <theviniciusluquini@gmail.com>
2025-03-29 19:27:43 -0300

    first commit

---
+++ src/main/java/com/lia/liaprove/LiaproveApplication.java
@@ -0,0 +1,13 @@
+package com.lia.liaprove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiaproveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiaproveApplication.class, args);
+	}
+
+}
