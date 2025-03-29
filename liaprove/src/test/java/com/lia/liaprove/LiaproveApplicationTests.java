4f6f62a6f9c20958842d945eb75aae447b71c780
Vinícius Luquini <theviniciusluquini@gmail.com>
2025-03-29 19:27:43 -0300

    first commit

---
+++ liaprove/src/test/java/com/lia/liaprove/LiaproveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lia.liaprove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiaproveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
