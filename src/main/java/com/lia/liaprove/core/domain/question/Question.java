91324bd9f5e07db222dc878bb2d7b7f14b7cf45e
Vinícius Luquini <theviniciusluquini@gmail.com>
2025-04-10 12:02:50 -0300

    feat: adding new class and refactoring attributes

--- src/main/java/com/lia/liaprove/core/domain/question/Question.java
+++ src/main/java/com/lia/liaprove/core/domain/question/Question.java
@@ -1,9 +1,7 @@
 package com.lia.liaprove.core.domain.question;
 
 import java.time.LocalDateTime;
-import java.util.EnumMap;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Question {
     private String title;
@@ -11,22 +9,44 @@ public class Question {
     private DifficultyLevel difficultyLevel;
     // A comunidade avaliará o nível de dificuldade da questão. Este atributo representa quantos votos cada nível recebeu.
     private EnumMap<DifficultyLevel, Integer> difficultyLevelVotes;
-    // A comunidade avaliará o nível de conhecimento da questão. Este atributo representa quantos votos cada nível recebeu.
-    private Map<String, Integer> knowledgeAreaVotes = new HashMap<>();
+    // A comunidade avaliará a área de conhecimento da questão. Este atributo representa quantos votos cada área recebeu.
+    private List<KnowledgeArea> knowledgeAreas = new ArrayList<>();
     // A comunidade avaliará a relevância da questão. Este atributo representa quantos votos cada nível recebeu.
-    private int[] relevanceVotes = new int[5]; // Relevância de 1 a 5
+    private int[] relevanceVotes = new int[5]; // De 1 a 5
     private LocalDateTime submissionDate;
     // Status da questão na plataforma
     private QuestionStatus status;
     // Número de votos totais que a questão recebeu
     private Integer totalVotes;
     private Boolean preEvaluatedByLLM;
-    // Nível de relevância, atribuído pela LLM
+    // Nível de relevância, atribuído pela LLM (1 a 5)
     private byte relevanceByLLM;
     private Boolean isAproved;
     private int upVote;
     private int downVote;
     // Número de vezes que o recruiter usou essa questão. Será utilizada no cálculo de sugestão pelas RBs.
     private int recruiterUsageCount;
+
+//    public void addKnowledgeArea(KnowledgeArea area) {
+//        knowledgeAreas.add(area);
+//    }
+//
+//    // Permitir votação em uma área de conhecimento específica
+//    public void voteKnowledgeArea(String areaName) {
+//        for (KnowledgeArea area : knowledgeAreas) {
+//            if (area.getName().equalsIgnoreCase(areaName)) {
+//                area.vote(); // Incrementa o contador de votos na área
+//                return;
+//            }
+//        }
+//        throw new IllegalArgumentException("Área de conhecimento não encontrada: " + areaName);
+//    }
+//
+//    // Exibir os votos para todas as áreas de conhecimento
+//    public void displayKnowledgeAreaVotes() {
+//        for (KnowledgeArea area : knowledgeAreas) {
+//            System.out.println(area);
+//        }
+//    }
 
 }
