b77133068800a3e6a84119536556ab591dd69675
Vinícius Luquini <theviniciusluquini@gmail.com>
2025-04-12 22:09:01 -0300

    feat: adding intermediate classes and refactoring

--- src/main/java/com/lia/liaprove/core/domain/assessment/Assessment.java
+++ src/main/java/com/lia/liaprove/core/domain/assessment/Assessment.java
@@ -1,11 +1,26 @@
 package com.lia.liaprove.core.domain.assessment;
 
+import com.lia.liaprove.core.domain.question.Question;
+import com.lia.liaprove.core.domain.relationship.AssessmentQuestion;
+
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class Assessment {
     private String title;
     private String description;
     // Como os Recruiters podem criar novas avaliações, a data de criação é uma informação importante
     private LocalDateTime creationDate;
+    private List<AssessmentQuestion> questions;
+    // Limite de tempo da avaliação
+    private Duration evaluationTimer;
+    // Data de expiração da avaliação (se necessário)
+    private LocalDateTime expirationDate;
+    // Total de vezes que a avaliação foi realizada
+    private int totalAttempts;
+    private AssessmentStatus status;
+    // Indica se é possível refazer a avaliação
+    private Boolean allowsRetake;
 
 }
---
+++ src/main/java/com/lia/liaprove/core/domain/relationship/UserAssessment.java
@@ -0,0 +1,12 @@
+package com.lia.liaprove.core.domain.relationship;
+
+import com.lia.liaprove.core.domain.assessment.Assessment;
+import com.lia.liaprove.core.domain.user.User;
+/*
+Esta classe representa o relacionamento de um usuário com uma avaliação.
+ */
+public class UserAssessment {
+    private User user;
+    private Assessment assessment;
+
+}
