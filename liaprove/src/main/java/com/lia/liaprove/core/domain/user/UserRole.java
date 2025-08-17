11f94f9521461a58889a5a363baf7723f8f986da
Vinícius Luquini <theviniciusluquini@gmail.com>
2025-08-17 11:05:10 -0300

    refactor: refactor Certificate and UserRole

--- liaprove/src/main/java/com/lia/liaprove/core/domain/user/UserRole.java
+++ liaprove/src/main/java/com/lia/liaprove/core/domain/user/UserRole.java
@@ -1,5 +1,17 @@
 package com.lia.liaprove.core.domain.user;
 
 public enum UserRole {
-    ADMIN, RECRUITER, PROFESSIONAL
+    ADMIN, RECRUITER, PROFESSIONAL;
+
+    public boolean isAdmin() {
+        return this == ADMIN;
+    }
+
+    public boolean isRecruiter() {
+        return this == RECRUITER;
+    }
+
+    public boolean isProfessional() {
+        return this == PROFESSIONAL;
+    }
 }
