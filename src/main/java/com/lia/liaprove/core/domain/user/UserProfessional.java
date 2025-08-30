d57126ffddf19930e365cef4f29dd0d6868bfd8f
Vinícius Luquini <theviniciusluquini@gmail.com>
2025-08-30 20:15:45 -0300

    feat: adding empty constructors in Users entities

--- src/main/java/com/lia/liaprove/core/domain/user/UserProfessional.java
+++ src/main/java/com/lia/liaprove/core/domain/user/UserProfessional.java
@@ -10,6 +10,8 @@ import java.util.UUID;
 
 public class UserProfessional extends User{
 
+    public UserProfessional() {}
+
     public UserProfessional(UUID id, String name, String email, String password, String occupation, String bio, ExperienceLevel experienceLevel, UserRole role, Integer voteWeight, Integer totalAssessmentsTaken, List<Certificate> certificates, Float averageScore, LocalDateTime registrationDate, LocalDateTime lastLogin) {
         super(id, name, email, password, occupation, bio, experienceLevel, role, voteWeight, totalAssessmentsTaken, certificates, averageScore, registrationDate, lastLogin);
     }
