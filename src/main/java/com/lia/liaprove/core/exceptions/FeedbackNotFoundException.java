be40a61039d8708fdb54bcd5a258b76089d57ea2
Vinícius Luquini <theviniciusluquini@gmail.com>
2025-11-04 11:33:55 -0300

    feat(exceptions): add metrics, assessment and question exceptions

---
+++ src/main/java/com/lia/liaprove/core/exceptions/FeedbackNotFoundException.java
@@ -0,0 +1,7 @@
+package com.lia.liaprove.core.exceptions;
+
+public class FeedbackNotFoundException extends RuntimeException {
+    public FeedbackNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/lia/liaprove/core/exceptions/QuestionNotFoundException.java
@@ -0,0 +1,7 @@
+package com.lia.liaprove.core.exceptions;
+
+public class QuestionNotFoundException extends RuntimeException {
+    public QuestionNotFoundException(String message) {
+        super(message);
+    }
+}
