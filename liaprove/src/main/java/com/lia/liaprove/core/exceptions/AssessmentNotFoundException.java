9b9c491284e6cee5ec25b76f072b3fcbeb1db3dd
Vinícius Luquini <theviniciusluquini@gmail.com>
2025-11-04 11:33:55 -0300

    feat(exceptions): add metrics, assessment and question exceptions

---
+++ liaprove/src/main/java/com/lia/liaprove/core/exceptions/AssessmentNotFoundException.java
@@ -0,0 +1,7 @@
+package com.lia.liaprove.core.exceptions;
+
+public class AssessmentNotFoundException extends RuntimeException {
+    public AssessmentNotFoundException(String message) {
+        super(message);
+    }
+}
