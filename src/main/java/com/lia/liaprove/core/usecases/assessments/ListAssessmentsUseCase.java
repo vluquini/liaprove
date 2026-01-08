3f023d248a57013d6106407da0370d7cca593749
Vinícius Luquini <theviniciusluquini@gmail.com>
2026-01-07 22:32:38 -0300

    chore(core:assessment): update difficultyLevel field reference to difficultyLevelByCommunity in Javadoc

--- src/main/java/com/lia/liaprove/core/usecases/assessments/ListAssessmentsUseCase.java
+++ src/main/java/com/lia/liaprove/core/usecases/assessments/ListAssessmentsUseCase.java
@@ -15,7 +15,7 @@ public interface ListAssessmentsUseCase {
     /**
      * Retorna uma lista de avaliações, potencialmente filtrada por critérios específicos.
      *
-     * @param filters Um mapa de filtros a serem aplicados (ex: "knowledgeArea", "difficultyLevel"). Pode ser nulo ou vazio.
+     * @param filters Um mapa de filtros a serem aplicados (ex: "knowledgeArea", "difficultyLevelByCommunity"). Pode ser nulo ou vazio.
      * @return Uma lista de objetos Assessment que correspondem aos filtros.
      */
     List<Assessment> execute(Map<String, Object> filters);
